89964b78904259dae453093a8799553fcf32e20e
bishwaTharu <bishwanaththaru34@gmail.com>
2024-02-06 16:26:38 +0545

    init

--- updated_rag (1).py
+++ updated_rag (1).py
@@ -179,15 +179,6 @@ class ChunkProcessor:
 
 chunk_processor = ChunkProcessor(file_path="./data", service_context=service_context)
 
-result = chunk_processor.process_chunks_and_query(prompt="what is supervised safety fine tuning?")
-print(result)
-
-result = chunk_processor.process_chunks_and_query(prompt="Can you tell me about the key concepts for safety finetuning")
-print(result)
-
-result = chunk_processor.process_chunks_and_query(prompt="what are the language models used in this research paper?")
-print(result)
-
 !curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
 !ngrok authtoken "Your-Ngrok-API"
